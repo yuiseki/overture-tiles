14206b69fd048a4e5fcb943af89412a9f2fbc546
Francisco Jimenez <jjcfrank@gmail.com>
2026-03-23 10:53:58 +0000

    feat: support bbox filtering with SOURCE_OVERRIDE
    Signed-off-by: Francisco Jimenez <jjcfrank@gmail.com>

--- bbox.sh
+++ bbox.sh
@@ -11,6 +11,7 @@ THEME=$3
 OUTPUT_DIR=$4
 OVERTURE_RELEASE_BUCKET=$5
 OVERTURE_REGION=$6
+INPUT_DIR=${7:-}  # Optional: local input directory (skips S3, used with SOURCE_OVERRIDE)
 
 # Parse bbox
 IFS=',' read -r MIN_LON MIN_LAT MAX_LON MAX_LAT <<< "$BBOX"
@@ -44,22 +45,27 @@ for TYPE in $TYPES; do
     mkdir -p "$TYPE_DIR"
 
     OUTPUT_FILE="$TYPE_DIR/filtered.parquet"
-    S3_PATH="$OVERTURE_RELEASE_BUCKET/$RELEASE/theme=$THEME/type=$TYPE/*.parquet"
+
+    if [ -n "$INPUT_DIR" ]; then
+        PARQUET_PATH="$INPUT_DIR/theme=$THEME/type=$TYPE/*.parquet"
+        S3_CONFIG=""
+    else
+        PARQUET_PATH="$OVERTURE_RELEASE_BUCKET/$RELEASE/theme=$THEME/type=$TYPE/*.parquet"
+        S3_CONFIG="SET s3_region='$OVERTURE_REGION'; SET s3_url_style='path';"
+    fi
 
     # Run DuckDB query
     duckdb -c "
     INSTALL spatial;
     LOAD spatial;
 
-    -- Configure for anonymous S3 access
-    SET s3_region='$OVERTURE_REGION';
-    SET s3_url_style='path';
+    $S3_CONFIG
 
     -- Query and filter data by bbox
     COPY (
         SELECT
             *
-        FROM read_parquet('$S3_PATH', union_by_name=true, filename=true, hive_partitioning=false)
+        FROM read_parquet('$PARQUET_PATH', union_by_name=true, filename=true, hive_partitioning=false)
         WHERE bbox.xmin <= $MAX_LON
           AND bbox.xmax >= $MIN_LON
           AND bbox.ymin <= $MAX_LAT
